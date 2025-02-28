23ee42f82c7a730357763bcb232c94a93c687cb0
Juan Puerta <juan.pablo.puerta@correounivalle.edu.co>
2025-02-28 14:15:20 -0500

    url reload

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <header>
             <nav class="nav-bar">
                 <div className="nav-div">
-                    <a href="http://localhost:5173" class="">
+                    <a href="https://3-d-organic-health.vercel.app" class="">
                     <img src="/images/logo.png" width="100" height="74" class="h-8" alt="Heart Wise Logo" />
                     <span className='business-name'>Heart Wise</span>
                     </a>
